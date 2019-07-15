f4ffa7925674f1177c9cce6c6830df4507ba07f8
Always、 <2199382542@qq.com>
2019-07-15 10:08:22 +0800

    启动类

---
+++ SpringBoot-Consumer/src/main/java/com/gaoh/consumer/SpringBootApplicationCustomer.java
@@ -0,0 +1,16 @@
+package com.gaoh.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoh
+ * @version 1.0
+ * @date 2019/7/15 10:05
+ */
+@SpringBootApplication
+public class SpringBootApplicationCustomer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationCustomer.class, args);
+    }
+}
---
+++ SpringBoot-Production/src/main/java/com/gaoh/production/SpringBootApplicationProduction.java
@@ -0,0 +1,16 @@
+package com.gaoh.production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoh
+ * @version 1.0
+ * @date 2019/7/15 10:05
+ */
+@SpringBootApplication
+public class SpringBootApplicationProduction {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationProduction.class, args);
+    }
+}
